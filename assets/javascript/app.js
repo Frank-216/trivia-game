979d0d8a999256d125945ae871b8e9ad577aff0d
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-06-01 21:20:25 -0400

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+var firstQuestion = 
+{
+	'questionNumber': 1,
+	'timeRemaining': 30,
+	'questionScript': "Which knight of Westeros is known as the Kingslayer?",
+	'potentialAnswers':
+	{
+		'1': 'Ned Stark',
+		'2':'Moonboy',
+		'3':'Jamie Lannister',
+		'4':'Euron Greyjoy'
+	},
+	"correctAnswer":
+	{
+		'3':'Jamie Lannister',
+	}
+}
+console.log(firstQuestion);
+var draftQuestion = 
+	{
+	'questionNumber': '3',
+	'timeRemaining': 30,
+	'questionScript': "Question?",
+	'potentialAnswers':
+	{
+		'1': 'Answer1',
+		'2':'answer2',
+		'3':'answer3',
+		'4':'answer4'
+	},
+	"correctAnswer":
+	{
+		'#':'answer#',
+	}
+}

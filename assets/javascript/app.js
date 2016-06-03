473bf51cd562fd789c6d276b0c9ea92af10850f5
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-06-02 21:03:30 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,32 +4,57 @@ var firstQuestion =
 	'timeRemaining': 30,
 	'questionScript': "Which knight of Westeros is known as the Kingslayer?",
 	'potentialAnswers':
-	{
-		'1': 'Ned Stark',
-		'2':'Moonboy',
-		'3':'Jamie Lannister',
-		'4':'Euron Greyjoy'
-	},
+	[
+	'Ned Stark',
+	'Moonboy',
+	'Jamie Lannister',
+	'Euron Greyjoy'
+	],
 	"correctAnswer":
 	{
 		'3':'Jamie Lannister',
 	}
 }
-console.log(firstQuestion);
+console.log(firstQuestion.questionScript);
 var draftQuestion = 
 	{
 	'questionNumber': '3',
 	'timeRemaining': 30,
 	'questionScript': "Question?",
 	'potentialAnswers':
-	{
-		'1': 'Answer1',
-		'2':'answer2',
-		'3':'answer3',
-		'4':'answer4'
-	},
+	[
+		'answer1',
+		'answer2',
+		'answer3',
+		'answer4'
+	],
 	"correctAnswer":
 	{
 		'#':'answer#',
 	}
 }
+//add my quote to my page every time 
+function render(){
+	var length = (firstQuestion.potentialAnswers.length);
+
+	for (var i = 0; i < length ; i ++){
+					var btn = $("<button>");
+					btn.addClass('col-md-6  btn-primary question'+ i);
+					btn.attr('data-let', firstQuestion.potentialAnswers[i]);
+					btn.html(firstQuestion.potentialAnswers[i]);
+					$(".start").append(btn);
+		}
+}
+$(document).ready(function(){
+	$(".start").on('click', function(){
+		console.log(firstQuestion.questionScript)
+		//first adjust the button 
+		$('.start').html(function(n){
+			return firstQuestion.questionScript;
+		})
+		render();
+		// Set the length to the length of my current question
+
+		
+	})
+})

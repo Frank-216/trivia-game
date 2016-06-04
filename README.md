8fd85402215f3152842b941212efc9f5be930207
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-06-03 22:59:53 -0400

    Update meow

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Trivia Game - Week 5 Assignment
 Thursday June 2 2016. 
  
  now i need to make a larger object & code to identify whether a question is correct aka a if statement that runs after button is clicked.  I need to add a specific class to these new btn class where i can check its value... compared to answer i also will need an array of objects which will be easier to call."
+
+ Firday June 3 2016.
+
+ Need to make two timer functions and a function that will adjust the valude of an array.  Which I will call question. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,11 +11,29 @@ var firstQuestion =
 	'Euron Greyjoy'
 	],
 	"correctAnswer":
-	{
-		'3':'Jamie Lannister',
-	}
+	[
+	'Jamie Lannister',
+	],
+	'imageGif':"https://media.giphy.com/media/VElt9te30u4Ks/giphy.gif",
 }
-console.log(firstQuestion.questionScript);
+var secondQuestion = 
+{
+	'questionNumber': 2,
+	'timeRemaining': 30,
+	'questionScript': "Who was the first King of The North in over 300 years?",
+	'potentialAnswers':
+	[
+	'Arya Stark',
+	'Ser Bronn of the BlackWater',
+	'Greyworm',
+	'Robb Stark'
+	],
+	"correctAnswer":
+	[
+	'Rob Stark',
+	],
+	'imageGif':"https://media.giphy.com/media/B4u41wAQ4KiTm/giphy.gif",
+}
 var draftQuestion = 
 	{
 	'questionNumber': '3',
@@ -33,28 +51,93 @@ var draftQuestion =
 		'#':'answer#',
 	}
 }
+var array = [firstQuestion, secondQuestion];
+console.log(array[1]);
+// counter for round Timer  
+var counter = 20; 
 //add my quote to my page every time 
 function render(){
 	var length = (firstQuestion.potentialAnswers.length);
 
 	for (var i = 0; i < length ; i ++){
 					var btn = $("<button>");
-					btn.addClass('col-md-6  btn-primary question'+ i);
+					btn.addClass('col-md-6  temp btn-primary question'+ i);
 					btn.attr('data-let', firstQuestion.potentialAnswers[i]);
 					btn.html(firstQuestion.potentialAnswers[i]);
-					$(".start").append(btn);
+					$("#start").append(btn);
 		}
+}// Close Render
+//addVideo takes information object Video URL and turns it into a new video 
+function delayNext(){
+	console.log("Bangers");
+	$('#start').empty();
+
+
 }
 $(document).ready(function(){
-	$(".start").on('click', function(){
-		console.log(firstQuestion.questionScript)
+	$('#start').on('click',function(){
+		var button = $('#start');
 		//first adjust the button 
-		$('.start').html(function(n){
-			return firstQuestion.questionScript;
+		button.html(function(n){
+			return "<h1> "+firstQuestion.questionScript +"</h1>";
 		})
 		render();
-		// Set the length to the length of my current question
+		//Set and display game 
+		var intervalID = setInterval(function(){
+			//reduce counter 
+			counter --; 
+			//display new counter value 
+			$('.timer').html("<h3>" + counter	+" </h2");
+			// if = user inform user that they have lost and call a function that will pick a new question. 
+			if(counter === 0 ){
+				alert( "Times UP");
+				clearInterval(intervalID);
+			}
+		}, 1000)
 
-		
 	})
+
+	$(document).on('click','.temp', function(){
+		// Hold Answer
+		var answer = firstQuestion.correctAnswer[0];
+		console.log(answer + " answer");
+		var userInput	= $(this).data("let");
+		console.log(userInput);
+		// Empty the Start Div 
+		$('#start').empty();
+		// Resent the information in the Div
+		var input = $('<div>');
+		input.addClass("col-md-12 result");
+		input.html(function(n){
+			return "<h1> "+firstQuestion.questionScript +"</h1>";
+		});
+
+		$('#start').append(input);
+		var gifUrl = firstQuestion.imageGif;
+		console.log(gifUrl);
+		var image = $("<img>");
+		image.attr('src', gifUrl);
+
+		$('.result').append(image);
+		// Sets the text informing the player whether they have won or lost 
+		if (userInput === answer){
+			var div = $("<div>");
+			div.addClass("col-md-12");
+			div.html('<h2>'+answer + " is correct! you have won this round.</h2>");
+			$(".result").append(div); 
+		}else{
+			var div = $("<div>");
+			div.addClass("col-md-12");
+			div.html('<h2>' + userInput + " is incorrect! The correctAnswer was " + answer + "! </h2>");
+			$('.result').append(div);
+			}
+			delayNext();
+
+		//Will empty the Temp
+		
+		})
+		// Set the length to the length of my current question
+	
+
+	
 })
